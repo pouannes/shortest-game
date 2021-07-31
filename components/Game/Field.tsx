76c73e225c8cd0f599d30aba5c1df93af62a6924
pouannes <pierreouannes@gmail.com>
2021-07-31 20:04:43 +0200

    add absolute imports and skeleton of Field

---
+++ components/Game/Field.tsx
@@ -0,0 +1,11 @@
+const Field = (): JSX.Element => {
+  return (
+    <div className="bg-gray-700 w-80 h-80">
+      <svg>
+        <rect />
+      </svg>
+    </div>
+  );
+};
+
+export default Field;
