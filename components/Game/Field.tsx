1ae1965bd5fc74c2e18584280cdf7b281d3f9767
pouannes <pierreouannes@gmail.com>
2021-08-02 01:34:08 +0200

    add human vs computer

--- components/Game/Field.tsx
+++ components/Game/Field.tsx
@@ -47,33 +47,36 @@ const Field = ({ columnNumber = 16 }: FieldProps): JSX.Element => {
   const cellSize = width / columnNumber;
   const rowNumber = Math.floor(height / cellSize);
 
-  const [grid, setGrid] = React.useState<GridType | null>(null);
+  const [humanGrid, setHumanGrid] = React.useState<GridType | null>(null);
+  const [computerGrid, setComputerGrid] = React.useState<GridType | null>(null);
 
   const [selectedCell, setSelectedCell] =
     React.useState<SelectedCellType | null>(null);
 
-  useMove({ grid, selectedCell, setSelectedCell, rowNumber, columnNumber });
-
-  const handleNextComputerMove = () => {
-    // if there's still cells to visit
-    if (
-      grid?.some((column) =>
-        column.some((cell) => cell.cellStatus !== "visited")
-      )
-    ) {
-      setGrid(_.clone(generator?.next()?.value));
-    }
-  };
+  useMove({
+    grid: humanGrid,
+    selectedCell,
+    setSelectedCell,
+    rowNumber,
+    columnNumber,
+  });
 
   // initialize maze
   React.useEffect(() => {
-    const smallGrid = generateMaze({
+    const smallHumanGrid = generateMaze({
       x: 0,
       y: 0,
       grid: getBaseGrid(rowNumber / 2, columnNumber / 2),
     });
+    setHumanGrid(increaseGridSize(smallHumanGrid));
 
-    setGrid(increaseGridSize(smallGrid));
+    const smallComputerGrid = generateMaze({
+      x: 0,
+      y: 0,
+      grid: getBaseGrid(rowNumber / 2, columnNumber / 2),
+    });
+    setComputerGrid(increaseGridSize(smallComputerGrid));
+
     setSelectedCell({
       x: Math.round(columnNumber / 2),
       y: Math.round(rowNumber / 2) - 1,
@@ -82,7 +85,7 @@ const Field = ({ columnNumber = 16 }: FieldProps): JSX.Element => {
 
   // mark the cell as visited when selectedCell moves
   React.useEffect(() => {
-    setGrid((prevGrid) =>
+    setHumanGrid((prevGrid) =>
       prevGrid
         ? prevGrid.map((column, columnIdx) =>
             columnIdx === selectedCell?.x
@@ -102,60 +105,126 @@ const Field = ({ columnNumber = 16 }: FieldProps): JSX.Element => {
     null
   );
   React.useEffect(() => {
-    if (grid && !generator) {
+    if (computerGrid && !generator) {
       setGenerator(
         depthSearchGenerator({
           x: Math.round(columnNumber / 2),
           y: Math.round(rowNumber / 2) - 1,
-          grid: _.clone(grid),
+          grid: _.clone(computerGrid),
         })
       );
     }
-  }, [columnNumber, generator, grid, rowNumber]);
+  }, [columnNumber, generator, computerGrid, rowNumber]);
+
+  const handleNextComputerMove = React.useCallback(() => {
+    // if there's still cells to visit
+    if (
+      computerGrid?.some((column) =>
+        column.some((cell) => cell.cellStatus !== "visited")
+      )
+    ) {
+      setComputerGrid(_.clone(generator?.next()?.value));
+    }
+  }, [computerGrid, generator]);
+
+  // trigger computer move when human discovers a new cell
+  const prevNumberHumanVisitedCells = React.useRef<number>(1);
+  React.useEffect(() => {
+    const numberHumanVisitedCells = humanGrid?.reduce(
+      (acc, currColumn) =>
+        acc +
+        currColumn.reduce(
+          (acc, currCell) =>
+            currCell.cellStatus === "visited" ? acc + 1 : acc,
+          0
+        ),
+      0
+    );
+    const numberComputerVisitedCells = computerGrid?.reduce(
+      (acc, currColumn) =>
+        acc +
+        currColumn.reduce(
+          (acc, currCell) =>
+            currCell.cellStatus === "visited" ? acc + 1 : acc,
+          0
+        ),
+      0
+    );
+    if (
+      typeof numberComputerVisitedCells === "number" &&
+      typeof numberHumanVisitedCells === "number"
+    ) {
+      if (
+        numberHumanVisitedCells > 0 &&
+        prevNumberHumanVisitedCells.current !== numberHumanVisitedCells
+      ) {
+        prevNumberHumanVisitedCells.current = numberHumanVisitedCells;
+        handleNextComputerMove();
+      } else if (numberComputerVisitedCells < numberHumanVisitedCells) {
+        handleNextComputerMove();
+      }
+    }
+  }, [computerGrid, handleNextComputerMove, humanGrid]);
 
   useKey(["n"], handleNextComputerMove);
 
   return (
-    <div className="relative bg-gray-800 border-2 border-gray-700 rounded-md">
-      <button
-        className="absolute left-0 p-3 bg-gray-400 rounded-md -top-20"
-        onClick={() =>
-          setGrid(
-            generateMaze({
-              x: 0,
-              y: 0,
-              grid: getBaseGrid(rowNumber, columnNumber),
-            })
-          )
-        }
-      >
-        Randomize walls
-      </button>
-      <button
-        className="absolute right-0 p-3 bg-gray-400 rounded-md -top-20"
-        onClick={handleNextComputerMove}
-      >
-        Next step
-      </button>
-      <svg width={width} height={height}>
-        {grid !== null ? (
-          <>
-            <Grid
-              grid={grid}
-              cellSize={cellSize}
-              setSelectedCell={setSelectedCell}
-            />
-            <Walls grid={grid} cellSize={cellSize} />
-            {selectedCell ? (
-              <SelectedCell
-                x={selectedCell.x}
-                y={selectedCell.y}
+    <div className="flex flex-col items-center justify-center">
+      <div className="relative bg-gray-800 border-2 border-gray-700 rounded-md">
+        {/* <button
+          className="absolute left-0 p-3 bg-gray-400 rounded-md -top-20"
+          onClick={() =>
+            setHumanGrid(
+              generateMaze({
+                x: 0,
+                y: 0,
+                grid: getBaseGrid(rowNumber, columnNumber),
+              })
+            )
+          }
+        >
+          Randomize walls
+        </button>
+        <button
+          className="absolute right-0 p-3 bg-gray-400 rounded-md -top-20"
+          onClick={handleNextComputerMove}
+        >
+          Next step
+        </button> */}
+        <svg width={width} height={height}>
+          {humanGrid !== null ? (
+            <>
+              <Grid
+                grid={humanGrid}
                 cellSize={cellSize}
+                setSelectedCell={setSelectedCell}
               />
-            ) : null}
-          </>
-        ) : null}
-      </svg>
+              <Walls grid={humanGrid} cellSize={cellSize} />
+              {selectedCell ? (
+                <SelectedCell
+                  x={selectedCell.x}
+                  y={selectedCell.y}
+                  cellSize={cellSize}
+                />
+              ) : null}
+            </>
+          ) : null}
+        </svg>
+      </div>
+      <div className="relative mt-4 bg-gray-800 border-2 border-gray-700 rounded-md">
+        <svg width={width} height={height}>
+          {computerGrid !== null ? (
+            <>
+              <Grid
+                grid={computerGrid}
+                cellSize={cellSize}
+                setSelectedCell={setSelectedCell}
+              />
+              <Walls grid={computerGrid} cellSize={cellSize} />
+            </>
+          ) : null}
+        </svg>
+      </div>
     </div>
   );
 };
